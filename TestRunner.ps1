9904eae322084355d3aaae891a6dd041cd6b584b
MattWalker414 <102538049+MattWalker414@users.noreply.github.com>
2023-04-06 12:27:48 -0400

    Update TestRunner.ps1
    Modified script to be used for both nightly/ci testing.

--- TestRunner.ps1
+++ TestRunner.ps1
@@ -1,6 +1,7 @@
-﻿Param(
+Param(
         [string]$source,
-        [string]$configPath
+        [string]$configPath,
+        [string]$testType
      )
 
 # Test areas to cycle through based Database name for the connection and folder name for tests (Database.Folder)...
@@ -11,52 +12,55 @@ $credential = New-Object System.Management.Automation.PsCredential("ssetestdom\b
 
 $testCafeDir = "$source\Testing\TestCafe\Scripts"
 
-cd $testCafeDir
-
-foreach ($test in $funcArea)
+function SetConfig
 {
-    $info = $test.split('.')
-    $database = $info[0]
-    $folder = $info[1]
-
-    # Edit the config file with database name...
-    Write-Host "********** IIS Shutting down for config edit... **********"
-    Invoke-Command -ComputerName QA1-12 -ScriptBlock { stop-service w3svc } -Credential $credential
-
-    $content = Get-Content -Path $configPath\Synergis.WebAPI\connections.config
-    $newContent = $content -replace ";initial catalog=(.*);persist", ";initial catalog=$database;persist"
-    Set-Content -Path $configPath\Synergis.WebAPI\connections.config -value $newContent
-
-    Write-Host "********** Config set to $database database connection.  Restarting IIS... **********"
-    Invoke-Command -ComputerName QA1-12 -ScriptBlock { Start-Service w3svc } -Credential $credential
+    Param (
+            [string]$dbname,
+            [string]$filePath,
+            [System.Management.Automation.PSCredential]$creds
+          )
     
-    # Run the tests
-    #testcafe chrome tests/qa/$folder/*.ts -r xunit:report/$database.xml
-    testcafe chrome tests/qa/$folder/check-out-dialog.ts -r xunit:report/$database.xml
+    # Edit the config file with database name...
+    Write-Host "********** IIS Shutting down for config edit for $dbname access... **********"
+    Invoke-Command -ComputerName QA1-12 -ScriptBlock { stop-service w3svc } -Credential $creds
 
-    <# This check should not be needed as the reporter setting will be ignored as a result of report setting on above command line.
-    # Not sure if QA using the TestCafe reporting setup so delete the report.xml
-    if (Test-Path $testCafeDir\report\report.xml) 
-    {
-        Remove-Item $testCafeDir\report\report.xml
-        Write-Host "Report.xml found and deleted."
-    }
-    else
-    {
-        Write-Host "Report.xml NOT found."
-    }
-    #>
+    $content = Get-Content -Path $filePath\Synergis.WebAPI\connections.config
+    $newContent = $content -replace ";initial catalog=(.*);persist", ";initial catalog=$dbname;persist"
+    Set-Content -Path $filePath\Synergis.WebAPI\connections.config -value $newContent
 
-    if ($lastexitcode -ne 0) { $global:lastexitcode = 0 }  # Ignore TestCafe script errors.
+    Write-Host "********** Config set to $dbname database connection.  Restarting IIS... **********"
+    Invoke-Command -ComputerName QA1-12 -ScriptBlock { Start-Service w3svc } -Credential $creds
+
 }
 
-Write-Host "********** IIS Shutting to restore Quality Db access... **********"
-Invoke-Command -ComputerName QA1-12 -ScriptBlock { stop-service w3svc } -Credential $credential
+cd $testCafeDir
 
-# Reset the test server config file to the Quality Db...
-$content = Get-Content -Path $configPath\Synergis.WebAPI\connections.config -Raw
-$newContent = $content -replace ";initial catalog=(.*);persist", ";initial catalog=Quality;persist"
-Set-Content -Path $configPath\Synergis.WebAPI\connections.config -value $newContent
+if ($testType -eq "Nightly")
+{
+    foreach ($test in $funcArea)
+    {
+        $info = $test.split('.')
+        $database = $info[0]
+        $folder = $info[1]
 
-Write-Host "********** Config reset to Quality database connection.  Restarting IIS... **********"
-Invoke-Command -ComputerName QA1-12 -ScriptBlock { Start-Service w3svc } -Credential $credential
+        SetConfig -dbname $database -configPath $configPath -creds $credential
+    
+        # Run the tests
+        #testcafe chrome tests/qa/$folder/*.ts -r xunit:report/$database.xml
+        testcafe chrome tests/qa/$folder/check-out-dialog.ts -r xunit:report/$database.xml        
+
+        if ($lastexitcode -ne 0) { $global:lastexitcode = 0 }  # Ignore TestCafe script errors.
+
+        SetConfig -dbname "Quality" -filePath $configPath -creds $credential                
+    }
+}
+else # CI Testing...
+{
+        # Edit the config file with Quality database name...
+        SetConfig -dbname "Quality" -filePath $configPath -creds $credential
+       
+        # Run the tests
+        testcafe chrome tests/CI/login-out/login-out.ts -r xunit:report/LoginOut.xml
+
+        if ($lastexitcode -ne 0) { $global:lastexitcode = 0 }  # Ignore TestCafe script errors
+}
