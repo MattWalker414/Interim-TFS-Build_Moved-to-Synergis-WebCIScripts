eadc5dd295ac276d725bb05217053172177cf5a0
MattWalker414 <102538049+MattWalker414@users.noreply.github.com>
2023-06-02 07:13:16 -0400

    Update TestRunner.ps1
    Commented the single test run.

--- TestRunner.ps1
+++ TestRunner.ps1
@@ -46,8 +46,8 @@ if ($testType -eq "Nightly")
         SetConfig -dbname $database -filePath $configPath -creds $credential
     
         # Run the tests...
-        #testcafe chrome tests/qa/$folder/*.ts -r xunit:report/$database.xml --browser-init-timeout 300000
-        testcafe chrome tests/qa/$folder/check-out-dialog.ts -r xunit:report/$database.xml --browser-init-timeout 300000      
+        testcafe chrome tests/qa/$folder/*.ts -r xunit:report/$database.xml --browser-init-timeout 300000
+        #testcafe chrome tests/qa/$folder/check-out-dialog.ts -r xunit:report/$database.xml --browser-init-timeout 300000      
 
         if ($lastexitcode -ne 0) { $global:lastexitcode = 0 }  # Ignore TestCafe script errors.
 
